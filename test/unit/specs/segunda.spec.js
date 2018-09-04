fb150ec9c047d1e77ff3699646edb4b15f816dca
Juan Fco Bustos <jbustos@devcenter.es>
2018-09-04 12:16:39 +0200

    Create test

--- test/unit/specs/segunda.spec.js
+++ test/unit/specs/segunda.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Segunda from '@/components/segunda'
 
-describe('HelloWorld.vue', () => {
+describe('segunda.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(Segunda)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+      .to.equal('Hola2')
   })
 })
